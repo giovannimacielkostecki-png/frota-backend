fbadea878fd8e2a71755144c276d7db13b5db126
giovannimacielkostecki-png <giovannimacielkostecki@gmail.com>
2026-06-02 13:54:04 -0300

    Update abastecimentoController.js

--- src/controllers/abastecimentoController.js
+++ src/controllers/abastecimentoController.js
@@ -55,6 +55,7 @@ async function criar(req, res, next) {
       ? parseFloat(((kmAtual - kmAnterior) / litros).toFixed(2))
       : null;
     const precoPorLitro = parseFloat((valorTotal / litros).toFixed(4));
+if (resto.data) resto.data = new Date(resto.data).toISOString();
 
     const [abastecimento] = await prisma.$transaction([
       prisma.abastecimento.create({
