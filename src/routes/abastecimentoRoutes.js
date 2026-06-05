0c9a232ccbf4708e5d210492052b2115037e17c3
giovannimacielkostecki-png <giovannimacielkostecki@gmail.com>
2026-06-05 14:00:00 -0300

    Update abastecimentoRoutes.js

--- src/routes/abastecimentoRoutes.js
+++ src/routes/abastecimentoRoutes.js
@@ -10,5 +10,7 @@ router.use(autenticar);
 router.get('/', ctrl.listar);
 router.get('/resumo', ctrl.resumoPorVeiculo);
 router.post('/', ctrl.criar);
+router.put('/:id',    auth, abastecimentoController.atualizar);
+router.delete('/:id', auth, abastecimentoController.deletar);
 
 export default router;
