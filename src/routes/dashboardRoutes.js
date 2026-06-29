9912381a39b33589b45d2423272e0a18f91c4068
giovannimacielkostecki-png <giovannimacielkostecki@gmail.com>
2026-06-29 09:56:05 -0300

    Update dashboardRoutes.js

--- src/routes/dashboardRoutes.js
+++ src/routes/dashboardRoutes.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-
 import ctrl from '../controllers/dashboardController.js';
-
 import { autenticar } from '../middlewares/auth.js';
 
 const router = Router();
@@ -9,9 +7,8 @@ const router = Router();
 router.use(autenticar);
 
 router.get('/resumo', ctrl.resumo);
-
 router.get('/custos-mensais', ctrl.custosMensais);
-
 router.get('/custo-veiculo', ctrl.custoPorVeiculo);
+router.get('/km-por-veiculo', ctrl.kmPorVeiculo);
 
 export default router;
